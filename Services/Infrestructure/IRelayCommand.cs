c32b9499aee95762de5a52c94655b80b92f7642f
Mauricio Salas Garcia <zS22013639@estudiantes.uv.mx>
2025-04-05 01:28:01 -0600

    Command services have been implemented

--- Services/Infrestructure/IRelayCommand.cs
+++ Services/Infrestructure/IRelayCommand.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Services.Infrestructure
 {
-    interface IRelayCommand
+    interface IRelayCommand : ICommand
     {
+        void RaiseCanExecuteChanged();
     }
 }
--- Services/Infrestructure/RelayCommand.cs
+++ Services/Infrestructure/RelayCommand.cs
@@ -1,12 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Services.Infrestructure
 {
-    class RelayCommand
+    class RelayCommand : ICommand, IRelayCommand
     {
+        private readonly Action<object> execute;
+        private readonly Predicate<object> canExecute;
+        private event EventHandler CanExecuteChangedInternal;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+                CanExecuteChangedInternal += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+                CanExecuteChangedInternal -= value;
+            }
+        }
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+
+        public RelayCommand(Action<object> execute) : this(execute, null)
+        {
+
+        }
+
+        public RelayCommand(Action execute)
+            : this(o => execute())
+        {
+        }
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null ? true : canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            execute(parameter);
+        }
+
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChangedInternal?.Invoke(this, EventArgs.Empty);
+            CommandManager.InvalidateRequerySuggested();
+        }
     }
 }
