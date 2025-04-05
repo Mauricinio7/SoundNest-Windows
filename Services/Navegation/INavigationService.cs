34ea052a5c19191735f4dcb7c1fbb562d6303d0c
Mauricio Salas Garcia <zS22013639@estudiantes.uv.mx>
2025-04-05 01:18:53 -0600

    Agregar archivos de proyecto.

---
+++ Services/Navegation/INavigationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Navegation
+{
+    public interface INavigationService
+    {
+        ViewModel CurrentView { get; }
+        void NavigateTo<T>() where T : ViewModel;
+        void NavigateTo<T>(object parameter) where T : ViewModel;
+    }
+}
---
+++ Services/Navegation/NavigationService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Navegation
+{
+    public class NavigationService : ObservableObject, INavigationService
+    {
+        private readonly Func<Type, ViewModel> viewModelFacory;
+        private ViewModel currentView;
+        public ViewModel CurrentView
+        {
+            get => currentView;
+            private set
+            {
+                currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public NavigationService(Func<Type, ViewModel> _viewModelFacory)
+        {
+            viewModelFacory = _viewModelFacory;
+        }
+
+        public void NavigateTo<TViewModel>() where TViewModel : ViewModel
+        {
+            ViewModel viewModel = viewModelFacory.Invoke(typeof(TViewModel));
+            CurrentView = viewModel;
+        }
+
+        public void NavigateTo<TViewModel>(object parameter) where TViewModel : ViewModel
+        {
+            ViewModel viewModel = viewModelFacory.Invoke(typeof(TViewModel));
+
+            if (viewModel is IParameterReceiver parameterReceiver)
+            {
+                parameterReceiver.ReceiveParameter(parameter);
+            }
+
+            CurrentView = viewModel;
+        }
+    }
+}
