f0c15c6dee04eeebf1554d8cfb7f364d83cbbb74
Navi <168604098+King-Navi@users.noreply.github.com>
2025-05-04 23:30:30 -0600

    Hallelujah How'd you do it? (How'd you do it?)You been on my mind

---
+++ Services/Communication/gRPC/Constants/ApiRoute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Communication.gRPC.Constants
+{
+    public class ApiRoute
+    {
+        public const string PORT = "50051";
+        public const string BaseUrl = $"https://localhost:{PORT}";
+
+    }
+}
